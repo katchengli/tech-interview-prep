945c20ee0794ca3e530cf51232e37134e8d3ed66
Katherine ChengLi <katchengli@gmail.com>
2017-02-27 17:46:49 -0800

    continuing avl tree implementation

--- data_structures/structures/avl_node.py
+++ data_structures/structures/avl_node.py
@@ -4,7 +4,9 @@ class AVLNode(Node):
     def __init__(self):
         super().__init__()
         self.balanceFactor = 0
+        self.parent = None
 
-    def __init__(self, value):
+    def __init__(self, parent, value):
         super().__init__(value)
         self.balanceFactor = 0
+        self.parent = parent
--- data_structures/structures/avl_tree.py
+++ data_structures/structures/avl_tree.py
@@ -6,8 +6,42 @@ class AVLTree(object):
 
     def insert(self, value):
         if self.root == None:
-            self.root = AVLNode(value)
+            self.root = AVLNode(None, value)
             return self.root
+
+        currentNode = self.root
+        while True:
+            if value < currentNode.value:
+                if currentNode.leftChild == None:
+                    currentNode.leftChild = AVLNode(currentNode, value)
+                    self.balanceTree(currentNode.leftChild)
+                else:
+                    currentNode = currentNode.leftChild
+            else:
+                if currentNode.rightChild == None:
+                    currentNode.rightChild = AVLNode(currentNode, value)
+                    self.balanceTree(currentNode.rightChild)
+                else:
+                    currentNode = currentNode.rightChild
+
+    def balanceTree(self, child):
+        parent = child.parent
+        if child.balanceFactor > 1 or child.balanceFactor < -1:
+            self.rebalanceTree(child)
+            return
+
+        if parent != None:
+            if child == parent.leftChild:
+                parent.balanceFactor += 1
+            elif child == parent.rightChild:
+                parent.balanceFactor -= 1
+
+            if parent.parent.balanceFactor != 0:
+                self.updateBalance(parent)
+
+    def updateBalance(self, node):
+
+
 
     def __str__(self):
         return str(self.root)
